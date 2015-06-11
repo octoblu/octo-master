1b04b9ccfdc35066f689eec58bc33c550ad24581
sqrt <sqrt@octoblu.com>
2015-06-11 16:46:42 -0700

    Attempt save state on stop

--- Dockerfile
+++ Dockerfile
@@ -1,19 +1,13 @@
 FROM node:0.10
 MAINTAINER Octoblu <docker@octoblu.com>
 
-EXPOSE 80
-
 RUN curl --silent -L https://github.com/coreos/fleet/releases/download/v0.10.0/fleet-v0.10.0-linux-amd64.tar.gz | tar -xz -C /opt/
+ENV PATH $PATH:/opt/fleet-v0.10.0-linux-amd64
 
 RUN mkdir -p /usr/src/app
 WORKDIR /usr/src/app
-
-COPY package.json /usr/src/app/
+COPY package.json  /usr/src/app/
 RUN npm install
-COPY *.js /usr/src/app/
-COPY *.coffee /usr/src/app/
-COPY *.eco /usr/src/app/
-
-ENV PATH $PATH:/opt/fleet-v0.10.0-linux-amd64
+COPY . /usr/src/app
 
 CMD ["npm", "start"]
--- docker_run.sh
+++ docker_run.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 
-docker build -t octoblu/octo-master:devel .
-
-docker stop octo-master &> /dev/null
-docker rm octo-master &> /dev/null
-docker run -it \
-  --name octo-master \
-  --detach \
-  -e DEBUG=octo-master* \
-  -e MESHBLU_DEVICE_UUID=0b6968f0-a147-4f95-a4d0-8a99a0a937f9 \
-  -e MESHBLU_DEVICE_TOKEN=0efe24cd5c8ee974dad8aeadffe15fa2d3c7223e \
-  -v /var/run/fleet.sock:/var/run/fleet.sock \
-  octoblu/octo-master:devel $@
+docker build -t octoblu/octo-master:latest .
+fleetctl destroy octo-master@.service
+fleetctl submit ./.systemd/octo-master@.service
+./rolling-restart.sh octo-master
