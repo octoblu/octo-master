501e9538f045105e7af0a20937b302ba71522243
sqrtofsaturn <sqrtofsaturn@gmail.com>
2015-06-11 11:09:18 -0700

    change secrets

--- rolling-restart.sh
+++ rolling-restart.sh
@@ -1,18 +1,18 @@
 #!/bin/sh
- 
+
 if [ "$#" -ne 1 ]; then
   echo "Usage: $0 your-service-prefix" >&2
   exit 1
 fi
- 
+
 fleetctl list-units | grep $1@ | cut -f1 -d. | while read -r unit ; do
   unit_index=`echo $unit | cut -f2 -d@`
- 
+
   printf "unit:> %s index:> %s\n" $unit $unit_index
-  
-  printf "stopping:> %s\n" $unit
-  fleetctl stop $unit
- 
+
+  printf "destroying:> %s\n" $unit
+  fleetctl destroy $unit
+
   printf "waiting:> for %s to stop " $unit;
   is_running=1
   while [ $is_running -ne 0 ]; do
@@ -21,10 +21,10 @@ fleetctl list-units | grep $1@ | cut -f1 -d. | while read -r unit ; do
     printf ".";
   done
   printf "\n"
- 
+
   printf "starting:> %s\n" $unit
   fleetctl start $unit
- 
+
   printf "waiting:> for %s to start " $unit;
   while [ $is_running -eq 0 ]; do
     is_running=`fleetctl list-units | grep running | grep $unit | wc -l`;
@@ -32,7 +32,7 @@ fleetctl list-units | grep $1@ | cut -f1 -d. | while read -r unit ; do
     printf ".";
   done
   printf "\n"
- 
+
   fleetctl list-units | grep $unit
-	
+
 done
